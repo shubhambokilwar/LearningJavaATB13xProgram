03e8cb8cc50b1b7337482049109e5721b7a90576
shubhambokilwar <shubhambokilwar@gmail.com>
2025-06-08 13:19:42 +0530

    New file Sum

---
+++ src/ex_01_Java_Basic/Lab002_Sum.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Sum {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println(c);
+    }
+}
