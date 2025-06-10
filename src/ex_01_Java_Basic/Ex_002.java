7b879f34c5e1804d468b8218381c398031c98814
shubhambokilwar <shubhambokilwar@gmail.com>
2025-06-10 08:57:06 +0530

    10 jUn

---
+++ src/ex_01_Java_Basic/Ex_002.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basic;
+
+public class Ex_002 {
+    public static void main(String[] args) {
+        int n = 5;
+
+            System.out.printf("%d X %d = %d%n", n, 1, 1 * n);
+            System.out.printf("%d X %d = %d%n", n, 2, 2 * n);
+           System.out.printf("%d X %d = %d%n", n, 3, 3 * n);
+            System.out.printf("%d X %d = %d%n", n, 4, 4 * n);
+            System.out.printf("%d X %d = %d%n", n, 5, 5 * n);
+            System.out.printf("%d X %d = %d%n", n, 6, 6 * n);
+            System.out.printf("%d X %d = %d%n", n, 7, 7 * n);
+            System.out.printf("%d X %d = %d%n", n, 8, 8 * n);
+            System.out.printf("%d X %d = %d%n", n, 9, 9 * n);
+            System.out.printf("%d X %d = %d%n", n, 10, 10 * n);
+
+
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Exercise_001.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Exercise_001 {
+    public static void main(String[] args) {
+        int n = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", n ,i, i * n);
+        }
+    }
+}
